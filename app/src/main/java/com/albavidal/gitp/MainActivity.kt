e5f6745a1d6ee4da10161e970e2e70233070b92d
AlbaVidal <vidalba97@gmail.com>
2023-04-18 15:51:33 +0200

    happy face

--- app/src/main/java/com/albavidal/gitp/MainActivity.kt
+++ app/src/main/java/com/albavidal/gitp/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            // :)
 
             GitPracticeTheme {
                 // A surface container using the 'background' color from the theme
